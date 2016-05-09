bd8325fb732634bfaa2daa967c8a4a5a6f5b1c3f
weihang <weihanglo@protonmail.com>
2016-05-09 10:07:48 +0800

    update setup config

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='py2048',
     version='0.0.1',
     description='A simple 2048 clone in Python TKinter',
-    url='https://github.com/wihanglo/py2048',
+    packages=['py2048', 'py2048.tests'],
+    url='https://github.com/weihanglo/py2048',
     author='Weihang Lo',
     author_email='weihanglo@protonmail.com',
 )
